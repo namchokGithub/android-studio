4f311d9c0849d838ac944b0917d86a65be00eab4
namchokGithub <pech4751@gmail.com>
2019-12-24 19:38:51 +0700

    login avenger

---
+++ LoginAvenger/app/src/main/java/com/example/loginavenger/Home.kt
@@ -0,0 +1,18 @@
+package com.example.loginavenger
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class Home : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
+    }
+}
