02ab344dd809535116bfa04e5874cb98295fc4eb
namchokGithub <pech4751@gmail.com>
2019-12-26 14:55:47 +0700

    edit input

--- loginfragment/app/src/main/java/com/example/loginfragment/MainActivity.kt
+++ loginfragment/app/src/main/java/com/example/loginfragment/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.loginfragment
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        try {
+            this.supportActionBar!!.hide()
+        } catch (e: NullPointerException) {
+        }
+
+        val fmLogin = login()
+        supportFragmentManager.beginTransaction().replace(R.id.contentFrame, fmLogin, "fmLogin").addToBackStack("fmLogin").commit()
+
+        val fmInput = loginInput()
+        supportFragmentManager.beginTransaction().replace(R.id.inputLogin, fmInput, "Input login").addToBackStack("Input login").commit()
+
     }
 }
--- loginfragment/app/src/main/java/com/example/loginfragment/login.kt
+++ loginfragment/app/src/main/java/com/example/loginfragment/login.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_main.*
 
 /**
  * A simple [Fragment] subclass.
@@ -16,8 +17,9 @@ class login : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        return view
     }
 
 
