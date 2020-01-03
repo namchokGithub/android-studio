9ae8416dcec35d2aac4ea082638fcea020c5aa29
namchokGithub <pech4751@gmail.com>
2020-01-03 17:30:11 +0700

    recipes.json

--- ListView/app/src/main/java/com/example/listview/MainActivity.kt
+++ ListView/app/src/main/java/com/example/listview/MainActivity.kt
@@ -12,5 +12,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment_list_view = ListView()
+        val manager = supportFragmentManager;
+        val transaction = manager.beginTransaction();
+        transaction.replace(R.id.layout, fragment_list_view,"fragment_list_view");
+        transaction.addToBackStack("fragment_list_view");
+        transaction.commit();
+
+
     }
 }
