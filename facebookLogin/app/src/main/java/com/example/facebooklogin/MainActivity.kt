73fee797b35074ae3e168b4738ad209579cefbaa
unknown <pech4751@gmail.com>
2020-01-18 23:09:37 +0700

    success login facebook in 3 sec

--- facebookLogin/app/src/main/java/com/example/facebooklogin/MainActivity.kt
+++ facebookLogin/app/src/main/java/com/example/facebooklogin/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.facebooklogin
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +22,23 @@ class MainActivity : AppCompatActivity() {
         transaction.addToBackStack("fragment_authen")
         transaction.commit()
 
+        debugHashKey()
     }
+
+    private fun debugHashKey() {
+        try {
+            val info = packageManager.getPackageInfo(
+                "com.example.facebooklogin",
+                PackageManager.GET_SIGNATURES
+            )
+            for (signature in info.signatures) {
+                val md: MessageDigest = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.d("KeyHash:", Base64.getEncoder().encodeToString(md.digest()))
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+        } catch (e: NoSuchAlgorithmException) {
+        }
+    }
+
 }
