64a0108e28c90f3a997f663dbcdbf31f27b68266
unknown <pech4751@gmail.com>
2020-01-18 22:54:43 +0700

    add fragment authen

--- facebookLogin/app/src/main/java/com/example/facebooklogin/MainActivity.kt
+++ facebookLogin/app/src/main/java/com/example/facebooklogin/MainActivity.kt
@@ -8,5 +8,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val authen = authen()
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+        transaction.replace(R.id.layout, authen,"fragment_authen")
+        transaction.addToBackStack("fragment_authen")
+        transaction.commit()
+
     }
 }
