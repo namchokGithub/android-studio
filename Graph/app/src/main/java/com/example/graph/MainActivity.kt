807f58a93762756a5875d6e091ee0c5231a9fa29
Namchok <pech4751@gmail.com>
2020-02-28 09:37:31 +0700

    Update main activity | Namchok

--- Graph/app/src/main/java/com/example/graph/MainActivity.kt
+++ Graph/app/src/main/java/com/example/graph/MainActivity.kt
@@ -11,5 +11,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val MainChart = MainChart()
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+        transaction.replace(R.id.contentContainer, MainChart,"fragment_MainChart")
+        transaction.addToBackStack("fragment_MainChart")
+        transaction.commit()
+
     }
+
 }
