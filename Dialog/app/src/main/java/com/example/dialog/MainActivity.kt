ae33fb559fa7f7f2e17e9bfebadf50f83117d207
Namchok <pech4751@gmail.com>
2020-03-06 18:03:31 +0700

    Finish Dialog

--- Dialog/app/src/main/java/com/example/dialog/MainActivity.kt
+++ Dialog/app/src/main/java/com/example/dialog/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.example.dialog
 
+import android.content.DialogInterface
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        // when button is clicked, show the alert
+        btnShowAlert.setOnClickListener {
+            // build alert dialog
+            val dialogBuilder = AlertDialog.Builder(this)
+
+            // set message of alert dialog
+            dialogBuilder.setMessage("Do you want to close this application ?")
+                    // if the dialog is cancelable
+                    .setCancelable(false)
+                    // positive button text and action
+                    .setPositiveButton("Proceed") { dialog, id -> finish()
+                    }
+                    // negative button text and action
+                    .setNegativeButton("Cancel") { dialog, id -> dialog.cancel()
+                    }
+
+            // create dialog box
+            val alert = dialogBuilder.create()
+            // set title for alert dialog box
+            alert.setTitle("AlertDialogExample")
+            // show alert dialog
+            alert.show()
+        }
     }
 }
