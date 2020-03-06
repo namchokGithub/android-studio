15c1e7470d229e514689c3abf5257dbb7000c5b1
Namchok <pech4751@gmail.com>
2020-03-06 16:57:13 +0700

    Test run v1

--- AsyncTask/app/src/main/java/com/example/asynctask/MainActivity.kt
+++ AsyncTask/app/src/main/java/com/example/asynctask/MainActivity.kt
@@ -8,5 +8,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ShowData = ShowData()
+        val manager = supportFragmentManager
+        val transaction = manager.beginTransaction()
+        transaction.replace(R.id.layout, ShowData,"fragment_ShowData")
+        transaction.addToBackStack("fragment_ShowData")
+        transaction.commit()
+
     }
+
 }
